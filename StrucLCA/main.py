ab2205a07c1006295510cc001f77264f946ec4c6
madswt <135027954+madswt@users.noreply.github.com>
2023-05-30 16:44:34 +0200

    Update main.py

--- StrucLCA/main.py
+++ StrucLCA/main.py
@@ -18,7 +18,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 def main():
-    st.title('#Make informed design decisions:heavy_exclamation_mark: ')
+    st.title('Make informed design decisions:heavy_exclamation_mark: ')
     st.write('Hello Tim :sunglasses:')
 
     beam_length = st.number_input('Span (meters):', min_value=0, step=1)
