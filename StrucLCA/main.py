5c9b90b025d706284723e7d3edca85cf09f3f5c4
madswt <135027954+madswt@users.noreply.github.com>
2023-05-30 16:34:54 +0200

    Update main.py

--- StrucLCA/main.py
+++ StrucLCA/main.py
@@ -19,6 +19,8 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def main():
     st.title('Make informed design decisions:heavy_exclamation_mark: ')
+    st.write('Hello Tim :sunglasses:
+             ')
 
     beam_length = st.number_input('Span (meters):', min_value=0, step=1)
     load = st.number_input('Load (kN/m):', min_value=0, step=1)
