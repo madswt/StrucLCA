ea5af3e620c5dae0bbe9ae068e2183ba79c69297
madswt <135027954+madswt@users.noreply.github.com>
2023-05-30 15:22:10 +0200

    Update main.py

--- StrucLCA/main.py
+++ StrucLCA/main.py
@@ -1,8 +1,8 @@
 import os
 import streamlit as st
 #from streamlit_echarts import st_echarts
-from steel import steel_calc
-from wood import wood_calc
+#from steel import steel_calc
+#from wood import wood_calc
 
 
 
@@ -23,8 +23,8 @@ def main():
 
     if st.button('Enter'):
         # Perform calculations
-        best_steel_profile, deflection = steel_calc(load, beam_length)
-        best_wood_profile = wood_calc(load, beam_length)
+        #best_steel_profile, deflection = steel_calc(load, beam_length)
+        #best_wood_profile = wood_calc(load, beam_length)
         
         
         # Generate chart
@@ -32,10 +32,12 @@ def main():
         options = {
             "xAxis": {
                 "type": "category",
-                "data": [best_steel_profile[0], best_wood_profile[1], "Result 3"],
+                "data": ['hej', 'hej', "Result 3"],
+                #"data": [best_steel_profile[0], best_wood_profile[1], "Result 3"],
             },
             "yAxis": {"type": "value"},
-            "series": [{"data": [best_steel_profile[1]*1e-6*beam_length*10, best_wood_profile[0]*10e-6*10,1], "type": "bar"}],
+            "series": [{"data": [1,1,1], "type": "bar"}]
+           # "series": [{"data": [best_steel_profile[1]*1e-6*beam_length*10, best_wood_profile[0]*10e-6*10,1], "type": "bar"}],
         }
         st_echarts(options=options, height="500px")
             
